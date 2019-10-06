9ab7b5598c927c0350dbb7f534c3850f9bf49be4
Birgerev <bibbevidio@gmail.com>
2019-10-06 22:05:14 +0200

    Logged Leaves

---
+++ Assets/Blocks/Logged_Leaves.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logged_Leaves : Block
+{
+    public override bool playerCollide { get; } = false;
+
+    public static string default_texture = "block_logged_leaves";
+    public override float breakTime { get; } = 3f;
+
+    public override Tool_Type propperToolType { get; } = Tool_Type.Axe;
+
+    public override ItemStack GetDrop()
+    {
+        return new ItemStack(Material.Oak_Log, 1);
+    }
+
+    public override void Tick()
+    {
+        base.Tick();
+    }
+}
