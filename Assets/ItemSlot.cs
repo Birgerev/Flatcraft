31f6b776f1a90e9eccdf02aa423dda16f20dcf12
Birgerev <bibbevidio@gmail.com>
2020-01-01 18:25:48 +0100

    Fixed inventory update spikes

--- Assets/ItemSlot.cs
+++ Assets/ItemSlot.cs
@@ -10,15 +10,18 @@ public class ItemSlot : MonoBehaviour
 
     public ItemStack item;
 
+    private CanvasGroup canvasGroup;
+
     // Start is called before the first frame update
     private void Start()
     {
+        canvasGroup = GetComponentInParent<CanvasGroup>();
     }
 
     // Start is called before the first frame update
     private void Update()
     {
-        if (Time.frameCount % 10 == 0)
+        if (Time.frameCount % 10 == 0 && canvasGroup.interactable)
         {
             UpdateSlot();
         }
