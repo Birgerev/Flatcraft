092886f0eef7443a974e7a2d5fb2754e9ec92305
Birgerev <bibbevidio@gmail.com>
2020-01-07 01:30:04 +0100

    fixed sheep entity

---
+++ Assets/Sheep.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : PassiveEntity
+{
+    //Entity Properties
+    public override float maxHealth { get; } = 8;
+
+    public override List<ItemStack> GetDrops()
+    {
+        //Drop a random amount of a certain item
+        List<ItemStack> result = new List<ItemStack>();
+        System.Random r = new System.Random(Chunk.seedByPosition(Vector2Int.CeilToInt(transform.position)));
+
+        result.Add(new ItemStack(Material.Wool_Block, r.Next(0, 3 + 1)));
+
+        return result;
+    }
+
+    public override EntityController GetController()
+    {
+        return new AnimalController(this);
+    }
+
+}
