4e9e5801fdf0480b09c5f98c0fdd2276f9e177c4
Birgerev <bibbevidio@gmail.com>
2020-01-28 23:11:29 +0100

    Relinked plank textures

--- Assets/Blocks/Oak_Planks.cs
+++ Assets/Blocks/Oak_Planks.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Oak_Planks : Block
 {
-    public static string default_texture = "block_Oak_Planks";
+    public static string default_texture = "block_oak_planks";
     public override float breakTime { get; } = 3f;
 
     public override Tool_Type propperToolType { get; } = Tool_Type.Axe;
