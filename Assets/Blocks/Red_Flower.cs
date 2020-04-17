31c6e840ed62aad2b8f43bc692538fbe299b328b
Birgerev <bibbevidio@gmail.com>
2020-04-17 13:40:18 +0200

    Fixed red flower in-inventory texture

--- Assets/Blocks/Red_Flower.cs
+++ Assets/Blocks/Red_Flower.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Red_Flower : Block
 {
-    public static string default_texture = "block_red_flower";
+    public static string default_texture = "block_red_flower_0";
     public override string[] alternative_textures { get; } = { "block_red_flower_0", "block_red_flower_1", "block_red_flower_2" };
 
     public override bool playerCollide { get; } = false;
