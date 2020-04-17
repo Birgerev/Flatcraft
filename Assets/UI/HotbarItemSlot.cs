095e451181c4333473cab4b3185ac51c44966093
Birgerev <bibbevidio@gmail.com>
2020-04-17 13:15:45 +0200

    Fixed ItemSlot durability Bar

--- Assets/UI/HotbarItemSlot.cs
+++ Assets/UI/HotbarItemSlot.cs
@@ -13,12 +13,11 @@ public class HotbarItemSlot : ItemSlot
     {
         if (Player.localInstance == null)
             return;
-
+        
         base.UpdateSlot();
 
         item = Player.localInstance.inventory.getHotbar()[transform.GetSiblingIndex()];
 
-        GetComponent<Image>().sprite = 
-(Player.localInstance.inventory.selectedSlot == transform.GetSiblingIndex()) ? selected : normal;
+        GetComponent<Image>().sprite = (Player.localInstance.inventory.selectedSlot == transform.GetSiblingIndex()) ? selected : normal;
     }
 }
