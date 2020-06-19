3806dcd6d87a91cbc99a9b04fc7202dad2f5f553
Birgerev <bibbevidio@gmail.com>
2020-06-19 15:34:03 +0200

    Decreased item pickup time

--- Assets/DroppedItem.cs
+++ Assets/DroppedItem.cs
@@ -43,7 +43,7 @@ public class DroppedItem : Entity
 
     private void OnTriggerStay2D(Collider2D col)
     {
-        if (!canPickup || age < 2)
+        if (!canPickup || age < 1.5f)
             return;
 
         if (col.GetComponent<DroppedItem>() != null)
