5fcd39d13b9af6691ef7fdd8c0c845571e727130
Birgerev <bibbevidio@gmail.com>
2020-10-19 16:45:38 +0200

    Fixed issue in save manager that would create patches of air in terrain

--- Assets/SaveManager.cs
+++ Assets/SaveManager.cs
@@ -24,7 +24,9 @@ public class SaveManager : MonoBehaviour
             //Save Block Changes
             if (blockChanges.Count > 0)
             {
-                var worldThread = new Thread(SaveBlockChanges);
+                var blockChangesCopy = new Dictionary<Location, string>(blockChanges);
+                blockChanges.Clear();
+                var worldThread = new Thread(() => { SaveBlockChanges(blockChangesCopy); });
                 worldThread.Start();
 
                 while (worldThread.IsAlive) yield return new WaitForSeconds(0.1f);
@@ -54,58 +56,49 @@ public class SaveManager : MonoBehaviour
             }
     }
 
-    public static void SaveBlockChanges()
+    public static void SaveBlockChanges(Dictionary<Location, string> changes)
     {
-        //Save Blocks
-        if (blockChanges.Count > 0)
-            lock (blockChanges)
+        //Get all changed chunks
+        var changedChunks = new List<ChunkPosition>();
+        foreach (var blockChange in changes)
+        {
+            var cPos = new ChunkPosition(blockChange.Key);
+            if (!changedChunks.Contains(cPos)) changedChunks.Add(cPos);
+        }
+
+        //Rewrite all block changes for chunks
+        foreach (var currentEditingChunk in changedChunks)
+        {
+            var changesForChunk = new Dictionary<Location, string>();
+            foreach (var blockChange in changes) //Add block change to list if it belongs to current chunk that we are editing
+                if (new ChunkPosition(blockChange.Key).Equals(currentEditingChunk))
+                    changesForChunk.Add(blockChange.Key, blockChange.Value);
+
+            if (!currentEditingChunk.HasBeenSaved()) currentEditingChunk.CreateChunkPath();
+
+            var chunkPath = WorldManager.world.getPath() + "\\region\\" + currentEditingChunk.dimension + "\\" +
+                            currentEditingChunk.chunkX;
+
+            foreach (var line in File.ReadAllLines(chunkPath + "\\blocks")
+            ) //Add all old block changes, removing duplicates of new block changes
             {
-                var changes = new Dictionary<Location, string>(blockChanges);
-                blockChanges.Clear();
+                var lineLoc = new Location(int.Parse(line.Split('*')[0].Split(',')[0]),
+                    int.Parse(line.Split('*')[0].Split(',')[1]));
+                var lineData = line.Split('*')[1] + "*" + line.Split('*')[2];
 
-                //Get all changed chunks
-                var changedChunks = new List<ChunkPosition>();
-                foreach (var blockChange in changes)
-                {
-                    var cPos = new ChunkPosition(blockChange.Key);
-                    if (!changedChunks.Contains(cPos)) changedChunks.Add(cPos);
-                }
-
-                //Rewrite all block changes for chunks
-                foreach (var currentEditingChunk in changedChunks)
-                {
-                    var changesForChunk = new Dictionary<Location, string>();
-                    foreach (var blockChange in changes
-                    ) //Add block change to list if it belongs to current chunk that we are editing
-                        if (new ChunkPosition(blockChange.Key).Equals(currentEditingChunk))
-                            changesForChunk.Add(blockChange.Key, blockChange.Value);
-
-                    if (!currentEditingChunk.HasBeenSaved()) currentEditingChunk.CreateChunkPath();
-
-                    var chunkPath = WorldManager.world.getPath() + "\\region\\" + currentEditingChunk.dimension + "\\" +
-                                    currentEditingChunk.chunkX;
-
-                    foreach (var line in File.ReadAllLines(chunkPath + "\\blocks")
-                    ) //Add all old block changes, removing duplicates of new block changes
-                    {
-                        var lineLoc = new Location(int.Parse(line.Split('*')[0].Split(',')[0]),
-                            int.Parse(line.Split('*')[0].Split(',')[1]));
-                        var lineData = line.Split('*')[1] + "*" + line.Split('*')[2];
-
-                        if (!changesForChunk.ContainsKey(lineLoc))
-                            changesForChunk.Add(lineLoc, lineData);
-                    }
-
-                    //Empty file before writing
-                    File.WriteAllText(chunkPath + "\\blocks", "");
-
-                    //Write all block changes
-                    TextWriter c = new StreamWriter(chunkPath + "\\blocks");
-                    foreach (var line in changesForChunk) c.WriteLine(line.Key.x + "," + line.Key.y + "*" + line.Value);
-
-                    //Close file writer
-                    c.Close();
-                }
+                if (!changesForChunk.ContainsKey(lineLoc))
+                    changesForChunk.Add(lineLoc, lineData);
             }
+
+            //Empty file before writing
+            File.WriteAllText(chunkPath + "\\blocks", "");
+
+            //Write all block changes
+            TextWriter c = new StreamWriter(chunkPath + "\\blocks");
+            foreach (var line in changesForChunk) c.WriteLine(line.Key.x + "," + line.Key.y + "*" + line.Value);
+
+            //Close file writer
+            c.Close();
+        }
     }
 }
