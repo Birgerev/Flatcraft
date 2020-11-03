322007d37b0554ffbc242ff93eef3a54033843b9
Birger Evansson <bibbevidio@gmail.com>
2020-11-03 12:55:11 +0100

    Cactie cant be placed beside other blocks

--- Assets/Blocks/Cactus.cs
+++ Assets/Blocks/Cactus.cs
@@ -6,4 +6,13 @@
     public override float breakTime { get; } = 0.65f;
 
     public override Block_SoundType blockSoundType { get; } = Block_SoundType.Wool;
+
+    public override void Tick()
+    {
+        if ((location + new Location(-1, 0)).GetMaterial() != Material.Air)
+            Break();
+        if ((location + new Location(1, 0)).GetMaterial() != Material.Air)
+            Break();
+        base.Tick();
+    }
 }
