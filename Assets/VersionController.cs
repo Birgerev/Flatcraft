d05508638040b5cb7d76426c5b36b2b3e2efc3e6
Birgerev <bibbevidio@gmail.com>
2020-11-10 23:20:55 +0200

    Main menu version

--- Assets/VersionController.cs
+++ Assets/VersionController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class VersionController : MonoBehaviour
 {
     public static int CurrentVersionId = 0;
-    public static List<string> versionNames = new List<string> { "indev 9" };
+    public static List<string> versionNames = new List<string> { "Indev 9" };
     
     public static string GetVersionName()
     {
