b50bffaa5ecb3b80e45e580418a423ec81877550
Birger Evansson <bibbevidio@gmail.com>
2020-12-18 00:20:51 +0100

    Door item texture

--- Assets/Items/Wooden_Door.cs
+++ Assets/Items/Wooden_Door.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class Wooden_Door : PlaceableItem
 {
+    public static string default_texture = "item_wooden_door";
     public override Material blockMaterial { get; } = Material.Wooden_Door_Bottom;
 }
