dbbba75ee59d4fd9b7f7da7a59742c905256e926
Birger Evansson <bibbevidio@gmail.com>
2021-01-03 20:15:48 +0100

    Netherrack block

---
+++ Assets/Blocks/Netherrack.cs
@@ -0,0 +1,9 @@
+﻿public class Netherrack : Block
+{
+    public override string texture { get; set; } = "block_netherrack";
+    public override float breakTime { get; } = 2;
+
+    public override Tool_Type propperToolType { get; } = Tool_Type.Pickaxe;
+    public override Tool_Level propperToolLevel { get; } = Tool_Level.Wooden;
+    public override Block_SoundType blockSoundType { get; } = Block_SoundType.Stone;
+}
