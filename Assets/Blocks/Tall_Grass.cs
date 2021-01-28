56bdde42965d8f9ff9da1b9e0ebb494868d32e25
Birgerev <bibbevidio@gmail.com>
2021-01-28 19:51:56 +0100

    Changed break times of more plants

--- Assets/Blocks/Tall_Grass.cs
+++ Assets/Blocks/Tall_Grass.cs
@@ -6,7 +6,7 @@
         {"block_tall_grass_0", "block_tall_grass_1", "block_tall_grass_2"};
 
     public override bool solid { get; set; } = false;
-    public override float breakTime { get; } = 0.05f;
+    public override float breakTime { get; } = 0.01f;
     public override bool requiresGround { get; } = true;
     public override bool isFlammable { get; } = true;
 
