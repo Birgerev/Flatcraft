aa6341272361ec9135cff54f57beb501a423427d
Birgerev <bibbevidio@gmail.com>
2021-01-30 00:40:46 +0100

    Removed grass block from background

--- Assets/BackgroundBlock.cs
+++ Assets/BackgroundBlock.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class BackgroundBlock : MonoBehaviour
 {
-    public static List<Material> viableMaterials = new List<Material> {Material.Stone, Material.Cobblestone, Material.Dirt, Material.Grass, Material.Oak_Planks, Material.Obsidian, Material.Sand, Material.Sandstone};
+    public static List<Material> viableMaterials = new List<Material> {Material.Stone, Material.Cobblestone, Material.Dirt, Material.Oak_Planks, Material.Obsidian, Material.Sand, Material.Sandstone};
     public Material material;
     
     // Start is called before the first frame update
