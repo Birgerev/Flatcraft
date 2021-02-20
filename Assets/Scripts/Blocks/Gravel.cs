e7a1e79cb623643302bd9309788c33454268c856
Birgerev <bibbevidio@gmail.com>
2021-02-20 01:54:28 +0100

    Gravel now doesn't drop flint same time as gravel

--- Assets/Scripts/Blocks/Gravel.cs
+++ Assets/Scripts/Blocks/Gravel.cs
@@ -5,14 +5,13 @@
 
     public override Tool_Type propperToolType { get; } = Tool_Type.Shovel;
     public override Block_SoundType blockSoundType { get; } = Block_SoundType.Gravel;
-
-
+    
     public override void Drop()
     {
-        new ItemStack(Material.Gravel, 1).Drop(location);
-
         if (new System.Random(SeedGenerator.SeedByLocation(location)).NextDouble() <= 0.1f)
             new ItemStack(Material.Flint, 1).Drop(location);
+        else
+            new ItemStack(Material.Gravel, 1).Drop(location);
     }
     
     public override void Tick()
