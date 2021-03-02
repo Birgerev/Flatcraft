467a8334b9b1ec1a9dfadb84c8e65e050175302c
Birgerev <bibbevidio@gmail.com>
2021-03-02 16:27:47 +0100

    Porkchop items

---
+++ Assets/Scripts/Items/Cooked_Porkchop.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Cooked_Porkchop : Food
+{
+    public override int food_points { get; } = 8;
+    public override string texture { get; set; } = "item_cooked_porkchop";
+}
