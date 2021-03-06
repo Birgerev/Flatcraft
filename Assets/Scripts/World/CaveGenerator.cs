a78bb0b22ec45398f009841b2e9fffc2990b2cab
Birgerev <bibbevidio@gmail.com>
2021-03-06 15:19:16 +0100

    Lower caves

--- Assets/Scripts/World/CaveGenerator.cs
+++ Assets/Scripts/World/CaveGenerator.cs
@@ -10,7 +10,7 @@ public class CaveGenerator : MonoBehaviour
     private const int perlinSnakeMaxAngleChange = 40;
     private const float perlinSnakeVericalAngleStopChance = 0.2f;
     
-    private const int maxCaveHeight = 60;
+    private const int maxCaveHeight = 50;
     
     public static void GenerateCavesForRegion(ChunkPosition chunkPos)
     {
