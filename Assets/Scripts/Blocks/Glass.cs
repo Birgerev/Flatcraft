7306b63a90b92d98c4a33befccdbb482247854a5
Birgerev <bibbevidio@gmail.com>
2021-04-11 22:25:00 +0200

    Glass blocks

---
+++ Assets/Scripts/Blocks/Glass.cs
@@ -0,0 +1,9 @@
+﻿public class Glass : Block
+{
+    public override string texture { get; set; } = "block_glass";
+    public override bool solid { get; set; } = true;
+    public override bool trigger { get; set; } = true;
+    public override float breakTime { get; } = 0.45f;
+
+    public override Block_SoundType blockSoundType { get; } = Block_SoundType.Glass;
+}
