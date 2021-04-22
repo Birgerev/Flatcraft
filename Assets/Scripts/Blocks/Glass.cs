2dace6b1edc2ac201c09316bc2004c8bc0f257ea
Birgerev <bibbevidio@gmail.com>
2021-04-23 00:43:35 +0200

    Glass now lights up more

--- Assets/Scripts/Blocks/Glass.cs
+++ Assets/Scripts/Blocks/Glass.cs
@@ -4,7 +4,7 @@
     public override bool solid { get; set; } = true;
     public override bool trigger { get; set; } = true;
     public override float breakTime { get; } = 0.45f;
-    public override int glowLevel { get; } = 8;
+    public override int glowLevel { get; } = 10;
 
     public override Block_SoundType blockSoundType { get; } = Block_SoundType.Glass;
 }
