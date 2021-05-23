efdfd193d4d658b5fe22cc4c91206f3db6eb42dd
Birgerev <bibbevidio@gmail.com>
2021-05-23 14:22:48 +0200

    Falling blocks now spawn at the correct y level

--- Assets/Scripts/Blocks/Sand.cs
+++ Assets/Scripts/Blocks/Sand.cs
@@ -1,4 +1,6 @@
-﻿public class Sand : Block
+﻿using UnityEngine;
+
+public class Sand : Block
 {
     public override string texture { get; set; } = "block_sand";
     public override float breakTime { get; } = 0.75f;
@@ -11,7 +13,7 @@
         if ((location + new Location(0, -1)).GetMaterial() == Material.Air)
         {
             var fs = (FallingBlock) Entity.Spawn("FallingBlock");
-            fs.transform.position = location.GetPosition();
+            fs.transform.position = location.GetPosition() - new Vector2(0, 0.5f);
             fs.material = GetMaterial();
 
             location.SetMaterial(Material.Air).Tick();
