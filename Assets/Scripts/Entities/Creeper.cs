ad1a44963e2459e54b64b80ada6af5854370748c
Birgerev <bibbevidio@gmail.com>
2021-05-31 02:14:07 +0200

    Creeper entity

--- Assets/Scripts/Entities/Creeper.cs
+++ Assets/Scripts/Entities/Creeper.cs
@@ -1,9 +1,16 @@
-using System;
 using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using Random = System.Random;
 
 public class Creeper : Monster
 {
-    public override float maxHealth { get; } = 10;
+    private float maxFuse = 1.5f;
+    [SyncVar] 
+    public float fuse;
+    [SyncVar] 
+    public bool ignited;
+    public override float maxHealth { get; } = 20;
 
     public override List<ItemStack> GetDrops()
     {
@@ -16,6 +23,46 @@ public class Creeper : Monster
         return result;
     }
 
+    [Server]
+    public override void Tick()
+    {
+        base.Tick();
+
+        if (ignited)
+        {
+            fuse += Time.deltaTime;
+        }
+
+        if (fuse >= maxFuse)
+            Explode();
+    }
+
+    [Server]
+    public void SetIgnited(bool ignited)
+    {
+        this.ignited = ignited;
+
+        if(!ignited)
+            fuse = 0;
+    }
+
+    [Server]
+    public void Explode()
+    {
+        Explosion.Create(Location, 4, 1);
+        Die();
+    }
+    
+    [Client]
+    public override void UpdateAnimatorValues()
+    {
+        base.UpdateAnimatorValues();
+        
+        Animator anim = GetComponent<Animator>();
+        
+        anim.SetBool("primed", ignited);
+    }
+    
     public override EntityController GetController()
     {
         return new CreeperController(this);
