2aa0b268a0a311a76af7ed88281680c0222021a1
Birgerev <bibbevidio@gmail.com>
2021-06-13 00:28:16 +0200

    Moved burn under sun to function

--- Assets/Scripts/Entities/Monster.cs
+++ Assets/Scripts/Entities/Monster.cs
@@ -8,14 +8,21 @@ public class Monster : LivingEntity
     {
         base.Tick();
 
-        if (burnUnderSun && WorldManager.GetTimeOfDay() == TimeOfDay.Day && Time.time % 10f - Time.deltaTime <= 0)
-        {
-            Block topmostBlock = Chunk.GetTopmostBlock(Location.x, Location.dimension, true);
-            if (Location.y > topmostBlock.location.y)
-            {
-                fireTime = 10;
-            } 
-        }
+        if (burnUnderSun)
+            TryBurnUnderSun();
+    }
+
+    private void TryBurnUnderSun()
+    {
+        if (WorldManager.GetTimeOfDay() != TimeOfDay.Day || Time.time % 10f - Time.deltaTime > 0)
+            return;
+                
+        Block topmostBlock = Chunk.GetTopmostBlock(Location.x, Location.dimension, true);
+
+        if (topmostBlock == null || Location.y > topmostBlock.location.y)
+            return;
+        
+        fireTime = 10;
     }
 
     public override void Hit(float damage, Entity source)
