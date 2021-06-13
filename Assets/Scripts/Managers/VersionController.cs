4a837dfdf7bc5320c2e900752f76f4181f7690d2
Birgerev <bibbevidio@gmail.com>
2021-06-13 18:58:36 +0200

    Simon test build

--- Assets/Scripts/Managers/VersionController.cs
+++ Assets/Scripts/Managers/VersionController.cs
@@ -5,7 +5,7 @@ public class VersionController : MonoBehaviour
 {
     public static List<string> versionNames = new List<string>
     {
-        "Indev 9", "Indev 10", "Indev 11", "Indev 12", "Indev 13", "Indev 14"
+        "Indev 9", "Indev 10", "Indev 11", "Indev 12", "Indev 13", "Indev 14 / Simon Test Build 1"
     };
 
     public static int CurrentVersionId => versionNames.Count - 1;
