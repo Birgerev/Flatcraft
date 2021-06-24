73415496d06c520295439fe705f20c246f369e3f
Birgerev <bibbevidio@gmail.com>
2021-06-24 14:57:54 +0200

    Format fixes

--- Assets/Scripts/Entities/Controllers/ZombieController.cs
+++ Assets/Scripts/Entities/Controllers/ZombieController.cs
@@ -1,10 +1,11 @@
 public class ZombieController : MonsterController
 {
-    public ZombieController(LivingEntity instance) : base(instance)
-    {
-    }
 
     protected override bool targetDamagerIfAttacked { get; } = true;
     protected override float targetRange { get; } = 16;
     protected override float hitTargetDamage { get; } = 4.5f;
+    
+    public ZombieController(LivingEntity instance) : base(instance)
+    {
+    }
 }
