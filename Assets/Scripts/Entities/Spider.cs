e2109c31040ef809fe2a0d244655332ee33118bb
Birgerev <bibbevidio@gmail.com>
2021-06-28 22:13:08 +0200

    Spider climbing

--- Assets/Scripts/Entities/Spider.cs
+++ Assets/Scripts/Entities/Spider.cs
@@ -1,5 +1,7 @@
-using System;
 using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using Random = System.Random;
 
 public class Spider : Monster
 {
@@ -16,6 +18,29 @@ public class Spider : Monster
         return result;
     }
 
+    public override void Tick()
+    {
+        base.Tick();
+
+        WallClimbCheck();
+    }
+
+    [Server]
+    private void WallClimbCheck()
+    {
+        Block rightBlock = Location.LocationByPosition(transform.position + new Vector3((GetWidth()/2) + 0.5f, 0)).GetBlock();
+        Block leftBlock = Location.LocationByPosition(transform.position + new Vector3(-((GetWidth()/2) + 0.5f), 0)).GetBlock();
+
+        //Climb when there is a block in front of entity
+        if ((rightBlock != null && rightBlock.solid) || (leftBlock != null && leftBlock.solid))
+        {
+            isOnClimbable = true;
+            return;
+        }
+        
+        isOnClimbable = false;
+    }
+    
     public override EntityController GetController()
     {
         return new SpiderController(this);
