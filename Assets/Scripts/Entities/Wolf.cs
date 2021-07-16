c644d822276f8751acd6e075b9530210e01b8de6
Birgerev <bibbevidio@gmail.com>
2021-07-17 01:46:37 +0200

    Wolf taming

--- Assets/Scripts/Entities/Wolf.cs
+++ Assets/Scripts/Entities/Wolf.cs
@@ -9,9 +9,31 @@ public class Wolf : PassiveEntity
     public override float maxHealth { get; } = 8;
     protected override float walkSpeed { get; } = 6f;
 
+    //TODO spawns
     public override EntityController GetController()
     {
         return new WolfController(this);
     }
     
+    [Server]
+    public override void Interact(Player source)
+    {
+        base.Interact(source);
+        
+        PlayerInventory inv = source.GetInventory();
+        ItemStack heldItem = inv.GetSelectedItem();
+
+        if (heldItem.material == Material.Bread)
+        {
+            heldItem.amount--;
+            inv.SetItem(inv.selectedSlot, heldItem);
+            Particle.Spawn_SmallSmoke(transform.position + new Vector3(0, 2), Color.red);
+            
+            Dog dog = (Dog)Entity.Spawn("Dog");
+            dog.Teleport(Location);
+            dog.ownerUuid = source.uuid;
+            
+            Remove();
+        }
+    }
 }
