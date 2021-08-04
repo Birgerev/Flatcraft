d88a6f80f16bb6619d5f87a0dac6435e2a4c3729
Birgerev <bibbevidio@gmail.com>
2021-08-04 13:39:05 +0200

    Moved tool click logic

--- Assets/Scripts/Items/Hoe.cs
+++ Assets/Scripts/Items/Hoe.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public class Hoe : Tool
 {
     public override Tool_Type tool_type { get; } = Tool_Type.Hoe;
+    
+    protected override void InteractRight(PlayerInstance player, Location loc, bool firstFrameDown)
+    {
+        if (loc.GetMaterial() == Material.Grass || loc.GetMaterial() == Material.Dirt)
+        {
+            loc.SetMaterial(Material.Farmland_Dry).Tick();
+            player.playerEntity.GetComponent<Player>().DoToolDurability();
+        }
+
+        base.InteractRight(player, loc, firstFrameDown);
+    }
 }
--- Assets/Scripts/Items/Tool.cs
+++ Assets/Scripts/Items/Tool.cs
@@ -10,31 +10,6 @@
         if (block != null)
             block.Hit(player, 1 / Player.interactionsPerPerSecond, tool_type, tool_level);
     }
-
-    protected override void InteractRight(PlayerInstance player, Location loc, bool firstFrameDown)
-    {
-        if (tool_type == Tool_Type.Hoe && (loc.GetMaterial() == Material.Grass || loc.GetMaterial() == Material.Dirt))
-        {
-            loc.SetMaterial(Material.Farmland_Dry).Tick();
-            player.playerEntity.GetComponent<Player>().DoToolDurability();
-        }
-
-        if (tool_type == Tool_Type.FlintAndSteel && loc.GetMaterial() == Material.Air)
-        {
-            loc.SetMaterial(Material.Fire).Tick();
-            player.playerEntity.GetComponent<Player>().DoToolDurability();
-            Sound.Play(loc, "random/flint_and_steel/click", SoundType.Entities, 0.8f, 1.2f);
-        }
-
-        if (tool_type == Tool_Type.FlintAndSteel && loc.GetMaterial() == Material.TNT)
-        {
-            ((TNT) loc.GetBlock()).Prime();
-            player.playerEntity.GetComponent<Player>().DoToolDurability();
-            Sound.Play(loc, "random/flint_and_steel/click", SoundType.Entities, 0.8f, 1.2f);
-        }
-
-        base.InteractRight(player, loc, firstFrameDown);
-    }
 }
 
 public enum Tool_Type
@@ -45,7 +20,6 @@ public enum Tool_Type
     , Shovel
     , Hoe
     , Sword
-    , FlintAndSteel
 }
 
 public enum Tool_Level
