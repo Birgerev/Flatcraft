b08d1c5fa2947eccefb60b0e1cd8be200c884d70
Birgerev <bibbevidio@gmail.com>
2021-08-09 15:21:01 +0200

    Stal disc item

---
+++ Assets/Scripts/Items/Music_Disc_Stal.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Disc_Stal : Item
+{
+    public override string texture { get; set; } = "item_disc_stal";
+}
