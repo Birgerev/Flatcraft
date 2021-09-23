5982bb025658ff6c76bd42bed8ac17cd50f9da7d
Birgerev <bibbevidio@gmail.com>
2021-09-23 16:42:11 +0200

    Dedicated server config support

--- Assets/Scripts/Managers/DedicatedServerManager.cs
+++ Assets/Scripts/Managers/DedicatedServerManager.cs
@@ -7,12 +7,23 @@ using UnityEngine.SceneManagement;
 public class DedicatedServerManager
 {
     private static string serverConfigPath = Application.dataPath + "/../serverConfig.dat";
+    public static Dictionary<string, string> configValues = new Dictionary<string, string>();
     
     public static bool DedicatedServerCheck()
     {
         //TODO ports, database
         if(File.Exists(serverConfigPath))
         {
+            string[] lines = File.ReadAllLines(serverConfigPath);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('=');
+                configValues.Add(parts[0], parts[1]);
+            }
+            
+            
+            
+            //Load or create a world
             World world = new World("world", new System.Random().Next());
             if (World.LoadWorlds().Count > 0)
                 world = World.LoadWorlds()[0];
