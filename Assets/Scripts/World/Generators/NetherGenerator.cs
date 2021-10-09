5102293a39b3830ca86e31651d785d319bfeeae8
Birgerev <bibbevidio@gmail.com>
2021-10-09 19:08:47 +0200

    New Nether generator

--- Assets/Scripts/World/Generators/NetherGenerator.cs
+++ Assets/Scripts/World/Generators/NetherGenerator.cs
@@ -2,38 +2,51 @@ using System;
 
 public class NetherGenerator : WorldGenerator
 {
-    public const int LavaLevel = 62;
+    public const int LavaLevel = 32;
 
     public override Material GenerateTerrainBlock(Location loc)
     {
+        if (loc.y > 128)
+            return Material.Air;
+        
         Random r = new Random(SeedGenerator.SeedByLocation(loc));
         ChunkPosition cPos = new ChunkPosition(loc);
         Biome biome = Biome.GetBiomeAt(cPos);
 
         Material mat = Material.Air;
 
-        //TODO
-        /*
         float noiseValue = biome.GetLandscapeNoiseAt(loc);
-        
-        
+
+        //-Netherrack generation-//
         if (noiseValue > 0.1f)
             mat = Material.Netherrack;
 
-        if (mat == Material.Air && loc.y <= LavaLevel) 
-            mat = Material.Lava;*/
-
-        if (loc.y <= 40)
+        //-Guaranteed Netherrack-//
+        if (loc.y >= 100)
             mat = Material.Netherrack;
+        if (loc.y <= 16)
+            mat = Material.Netherrack;
+        
+        //TODO fortress
+        
 
+        //-Caves-//
+        if (WorldManager.instance.caveHollowBlocks.Contains(loc))
+            mat = Material.Air;
+
+        //-Lava Sea-//
+        if (mat == Material.Air && loc.y <= LavaLevel)
+            mat = Material.Lava;
+
+        //-Random Lava-//
+        if(mat == Material.Netherrack && r.NextDouble() < 0.003d)
+            mat = Material.Lava;
 
         //-Lower Bedrock Generation-//
         if (loc.y <= 4)
         {
             //Fill layer 0 and then progressively less chance of bedrock further up
-            if (loc.y == 0)
-                mat = Material.Bedrock;
-            else if (r.Next(0, loc.y + 2) <= 1)
+            if (r.Next(0, loc.y + 2) <= 1)
                 mat = Material.Bedrock;
         }
 
@@ -41,15 +54,10 @@ public class NetherGenerator : WorldGenerator
         if (loc.y >= 128 - 4 && loc.y <= 128)
         {
             //Fill layer 256 and then progressively less chance of bedrock further down
-            if (loc.y == 128 - 4)
-                mat = Material.Bedrock;
-            else if (r.Next(0, 128 - loc.y + 2) <= 1)
+            if (r.Next(0, 128 - loc.y + 2) <= 1)
                 mat = Material.Bedrock;
         }
-
-        if (loc.y > 128)
-            mat = Material.Air;
-
+        
         return mat;
     }
 }
