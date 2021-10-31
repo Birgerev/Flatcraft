d938c2f443c17428c6ce2c92c00a39a4ec88a577
Birgerev <bibbevidio@gmail.com>
2021-10-31 12:51:32 +0100

    Inventory Container Loottable support

--- Assets/Scripts/Blocks/InventoryContainer.cs
+++ Assets/Scripts/Blocks/InventoryContainer.cs
@@ -1,13 +1,29 @@
-﻿public class InventoryContainer : Block
+﻿using System;
+
+public class InventoryContainer : Block
 {
     public override void ServerInitialize()
     {
         base.ServerInitialize();
 
+        //Create inventory if none has been assigned
         if (!GetData().HasTag("inventoryId"))
         {
             Inventory inv = NewInventory();
             location.SetData(GetData().SetTag("inventoryId", inv.id.ToString()));
+        }
+        //Fill with items if we have a loottable
+        if (GetData().HasTag("loottable"))
+        {
+            string loottableName = GetData().GetTag("loottable");
+            Loottable loottable = Loottable.Load(loottableName);
+            Inventory inv = GetInventory();
+            Random random = new Random();
+            foreach (ItemStack item in loottable.GetRandomItems())
+            {
+                inv.SetItem(random.Next(0, inv.size), item);
+            }
+            location.SetData(GetData().RemoveTag("loottable"));
         }
 
         GetInventory();
