fda8ac7ee56d2d96f3eda9633cbde23c45952a03
Birgerev <bibbevidio@gmail.com>
2021-11-05 19:10:37 +0100

    Melons need groud

--- Assets/Scripts/Blocks/Melon.cs
+++ Assets/Scripts/Blocks/Melon.cs
@@ -5,6 +5,7 @@ public class Melon : Block
     public override string texture { get; set; } = "block_melon";
     public override float breakTime { get; } = 3f;
     public override bool isFlammable { get; } = true;
+    public override bool requiresGround { get; } = true;
 
     public override Tool_Type properToolType { get; } = Tool_Type.Axe;
     public override Block_SoundType blockSoundType { get; } = Block_SoundType.Wood;
