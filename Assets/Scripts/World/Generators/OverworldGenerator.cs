eb26fbebe54ad460f290156116b7b54b3e39cfcc
Birgerev <bibbevidio@gmail.com>
2021-11-06 11:56:10 +0100

    Update OverworldGenerator.cs

--- Assets/Scripts/World/Generators/OverworldGenerator.cs
+++ Assets/Scripts/World/Generators/OverworldGenerator.cs
@@ -5,6 +5,8 @@ using Random = System.Random;
 
 public class OverworldGenerator : WorldGenerator
 {
+    public static int SeaLevel = 62;
+    
     //Buy the game god damn it
     public OverworldGenerator()
     {
