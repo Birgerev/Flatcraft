57f96a28ddfaf3521c5a05361c6ae0530335b9d4
Birgerev <bibbevidio@gmail.com>
2021-11-06 11:46:45 +0100

    Skyblock further up

--- Assets/Scripts/World/Generators/SkyblockGenerator.cs
+++ Assets/Scripts/World/Generators/SkyblockGenerator.cs
@@ -7,13 +7,13 @@ public class SkyblockGenerator : WorldGenerator
 
     public override BlockState GenerateStructures(Location loc, Biome biome)
     {
-        if (loc.x == 0 && loc.y == 60 && loc.dimension == Dimension.Overworld)
+        if (loc.x == 0 && loc.y == 80 && loc.dimension == Dimension.Overworld)
             return new BlockState(Material.Structure_Block, new BlockData("structure=Sky_Block/Grass"));
         
-        if (loc.x == 65 && loc.y == 60 && loc.dimension == Dimension.Overworld)
+        if (loc.x == 65 && loc.y == 80 && loc.dimension == Dimension.Overworld)
             return new BlockState(Material.Structure_Block, new BlockData("structure=Sky_Block/Sand"));
         
-        if (loc.x == 0 && loc.y == 60 &&  loc.dimension == Dimension.Nether)
+        if (loc.x == 0 && loc.y == 80 &&  loc.dimension == Dimension.Nether)
             return new BlockState(Material.Structure_Block, new BlockData("structure=Sky_Block/Nether"));
 
         return new BlockState(Material.Air);
