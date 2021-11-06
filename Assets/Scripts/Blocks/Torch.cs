a668ebdf5e5be0f54724bb6ad6c1abe606314310
Birgerev <bibbevidio@gmail.com>
2021-11-06 20:32:17 +0100

    Torch animation

--- Assets/Scripts/Blocks/Torch.cs
+++ Assets/Scripts/Blocks/Torch.cs
@@ -1,6 +1,11 @@
 ﻿public class Torch : Block
 {
-    public override string texture { get; set; } = "block_torch";
+    public override string texture { get; set; } = "block_torch_0";
+    public override string[] alternativeTextures { get; } =
+    {
+        "block_torch_0", "block_torch_1", "block_torch_2", "block_torch_3"
+    };
+    public override float changeTextureTime { get; } = 0.3f;
     public override bool solid { get; set; } = false;
     public override float breakTime { get; } = 0.1f;
 
