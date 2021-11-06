e7c8f5febd8f6a42f463e969cf6958c276df7f7c
Birgerev <bibbevidio@gmail.com>
2021-11-06 11:38:54 +0100

    More fuels

--- Assets/Scripts/Player/Inventory/SmeltingRecipe.cs
+++ Assets/Scripts/Player/Inventory/SmeltingRecipe.cs
@@ -8,7 +8,20 @@ public class SmeltingRecipe
 
     public static Dictionary<Material, float> Fuels = new Dictionary<Material, float>
     {
-        {Material.Coal, 80}
+        {Material.Coal, 80},
+        {Material.Lava_Bucket, 1000},
+        {Material.Oak_Log, 15},
+        {Material.Birch_Log, 15},
+        {Material.Oak_Planks, 15},
+        {Material.Ladder, 15},
+        {Material.Crafting_Table, 15},
+        {Material.Chest, 15},
+        {Material.Wooden_Axe, 10},
+        {Material.Wooden_Pickaxe, 10},
+        {Material.Wooden_Hoe, 10},
+        {Material.Wooden_Shovel, 10},
+        {Material.Wooden_Sword, 10},
+        {Material.Oak_Sapling, 5},
     };
 
     public Material ingredient = Material.Air;
