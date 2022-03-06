b913095b8006099ea963d979198428da74db2acc
Birgerev <bibbevidio@gmail.com>
2022-03-06 23:35:21 +0100

    Minor refractor

--- Assets/Scripts/World/Decoration/OverworldSky.cs
+++ Assets/Scripts/World/Decoration/OverworldSky.cs
@@ -2,26 +2,25 @@
 
 public class OverworldSky : MonoBehaviour
 {
-    public GameObject Moon;
-
     public GameObject playerLockedY;
     public GameObject skySpinner;
-    public GameObject Sun;
+    public GameObject sun;
+    public GameObject moon;
 
-    private float timeOfDay;
+
 
     // Update is called once per frame
     private void Update()
     {
+        float timeOfDay01 = WorldManager.instance.worldTime % WorldManager.DayLength / WorldManager.DayLength;
+            
         Vector3 cameraPosition = CameraController.instance.transform.position;
-
         playerLockedY.transform.position = new Vector3(transform.position.x, cameraPosition.y);
-        skySpinner.transform.rotation = Quaternion.Euler(0, 0, -timeOfDay * 360);
-
-        Sun.transform.rotation = Quaternion.identity;
-        Moon.transform.rotation = Quaternion.identity;
-
-        timeOfDay = WorldManager.instance.worldTime % WorldManager.DayLength / WorldManager.DayLength;
-        GetComponent<Animator>().SetFloat("time", timeOfDay * 100);
+        
+        skySpinner.transform.rotation = Quaternion.Euler(0, 0, -timeOfDay01 * 360);
+        sun.transform.rotation = Quaternion.identity;
+        moon.transform.rotation = Quaternion.identity;
+        
+        GetComponent<Animator>().SetFloat("time", timeOfDay01 * 100);
     }
 }
