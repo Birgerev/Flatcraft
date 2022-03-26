838d02993d94eaf7e70c43622e6697028b6da3d1
Birgerev <bibbevidio@gmail.com>
2022-03-26 14:09:25 +0100

    Fullscreen options

--- Assets/UI/Scripts/OptionsMenu.cs
+++ Assets/UI/Scripts/OptionsMenu.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public Text fullscreenText;
+
+    private void Update()
+    {
+        fullscreenText.text = "Fullscreen: " + (Screen.fullScreen ? "On" : "Off");
+    }
+
     public void ToggleFullscreen()
     {
-        //TODO
+        Screen.fullScreen = !Screen.fullScreen;
     }
     
     public void Close()
