a82412d18af1688d1eae20aea75ec19ab01d4755
Birgerev <bibbevidio@gmail.com>
2022-03-26 13:07:46 +0100

    Options Draft

---
+++ Assets/UI/Scripts/OptionsMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public void Close()
+    {
+        Destroy(gameObject);
+    }
+}
