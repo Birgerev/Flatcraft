50036ce4a0eb508f50e3895eb98fc5e7a57f789e
Birgerev <bibbevidio@gmail.com>
2022-06-18 01:25:13 +0200

    Unity version 2021.3.4f1

--- Assets/UI/Scripts/MultiplayerMenu.cs
+++ Assets/UI/Scripts/MultiplayerMenu.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Policy;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
