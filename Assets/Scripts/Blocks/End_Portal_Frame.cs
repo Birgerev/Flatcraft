40300ed09b31846595711c0cc2cd1ef571b32a8c
Birgerev <bibbevidio@gmail.com>
2022-11-03 14:03:23 +0100

    End portal frame block

---
+++ Assets/Scripts/Blocks/End_Portal_Frame.cs
@@ -0,0 +1,7 @@
+﻿public class End_Portal_Frame : Block
+{
+    public override string texture { get; set; } = "block_end_portal_frame";
+    public override float breakTime { get; } = float.PositiveInfinity;
+
+    public override Block_SoundType blockSoundType { get; } = Block_SoundType.Stone;
+}
