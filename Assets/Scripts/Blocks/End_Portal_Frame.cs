d051a9c0d3b483800356fafe22ae223145f7d744
Birgerev <bibbevidio@gmail.com>
2022-11-03 14:07:37 +0100

    Fixed error

--- Assets/Scripts/Blocks/End_Portal_Frame.cs
+++ Assets/Scripts/Blocks/End_Portal_Frame.cs
@@ -1,7 +1,7 @@
 ﻿public class End_Portal_Frame : Block
 {
     public override string texture { get; set; } = "block_end_portal_frame";
-    public override float breakTime { get; } = float.PositiveInfinity;
+    public override float breakTime { get; } = 100000000;
 
     public override Block_SoundType blockSoundType { get; } = Block_SoundType.Stone;
 }
