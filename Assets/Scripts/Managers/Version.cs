3edf06386231ab7d533c825ed916f633af9d0aa5
Birgerev <bibbevidio@gmail.com>
2022-12-06 19:13:58 +0100

    Update Version.cs

--- Assets/Scripts/Managers/Version.cs
+++ Assets/Scripts/Managers/Version.cs
@@ -6,7 +6,7 @@ public class Version : MonoBehaviour
     public readonly static List<string> versions = new List<string>
     {
         "Indev 9", "Indev 10", "Indev 11", "Indev 12", "Indev 13", "Indev 14 / simon test 1", "Indev 14 / simon test 2", 
-        "Alpha 1.0 Candidate 1", "Alpha 1.0 Demo", "Alpha 1.0", "Alpha 1.1 Candidate 1"
+        "Alpha 1.0 Candidate 1", "Alpha 1.0 Demo", "Alpha 1.0", "Alpha 1.1 Candidate 1", "Alpha 1.2"
     };
 
     public static int currentId => versions.Count - 1;
