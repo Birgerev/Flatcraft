7faec94f42f56828b26e761311cde41bf16db628
Birgerev <bibbevidio@gmail.com>
2022-12-06 19:24:08 +0100

    New monster spawning

--- Assets/Scripts/World/Chunk/Chunk.cs
+++ Assets/Scripts/World/Chunk/Chunk.cs
@@ -25,7 +25,7 @@ public class Chunk : NetworkBehaviour
     
     private static readonly int monsterSpawningLightLevel = 7;
     private static readonly int monsterSpawnAmountCap = 1;
-    private static readonly List<string> DefaultOverworldMonsters = new List<string> {"Zombie", "Creeper", "Spider", "Skeleton"};
+    private static readonly List<string> DefaultOverworldMonsters = new List<string> {"Zombie", "Creeper", "Spider", "Skeleton", "Enderman"};
 
     public GameObject blockPrefab;
     public GameObject backgroundBlockPrefab;
