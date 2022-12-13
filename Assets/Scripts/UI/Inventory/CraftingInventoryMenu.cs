d859cbed898dd5651e4e3a7ec4393af6c4ddb0f4
Birger E <bibbevidio@gmail.com>
2022-12-13 16:05:36 +0100

    Crafting table shift click

--- Assets/Scripts/UI/Inventory/CraftingInventoryMenu.cs
+++ Assets/Scripts/UI/Inventory/CraftingInventoryMenu.cs
@@ -4,22 +4,25 @@ public class CraftingInventoryMenu : ContainerInventoryMenu
 {
     public override void UpdateInventory()
     {
-        CheckCraftingRecipes();
+        RefreshCraftingResultSlot();
         base.UpdateInventory();
     }
 
     [Server]
-    public void CheckCraftingRecipes()
+    public void RefreshCraftingResultSlot()
     {
         CraftingInventory inv = (CraftingInventory) Inventory.Get(inventoryIds[0]);
+        //Try to find a matching recipe
         CraftingRecipe curRecipe = CraftingRecipe.FindRecipeByItems(inv.GetCraftingTableItems());
 
+        //If no matching recipe was found, clear result slot
         if (curRecipe == null)
         {
             inv.SetItem(inv.GetCraftingResultSlot(), new ItemStack());
             return;
         }
 
+        //Otherwise fill result slot
         inv.SetItem(inv.GetCraftingResultSlot(), curRecipe.result);
     }
 
@@ -30,7 +33,7 @@ public class CraftingInventoryMenu : ContainerInventoryMenu
 
         if (inventoryIndex == 0 && slotIndex == inv.GetCraftingResultSlot())
         {
-            OnClickCraftingResultSlot();
+            OnClickCraftingResultSlot(clickType);
             return;
         }
 
@@ -38,25 +41,37 @@ public class CraftingInventoryMenu : ContainerInventoryMenu
     }
 
     [Command(requiresAuthority = false)]
-    public virtual void OnClickCraftingResultSlot()
+    public virtual void OnClickCraftingResultSlot(ClickType clickType)
+    {
+        if (clickType == ClickType.ShiftClick)
+            SlotAction_TransferResultSlotUntilEmpty();
+        else
+            SlotAction_GrabResultSlot();
+    }
+    
+    [Server]
+    public virtual void SlotAction_GrabResultSlot()
     {
         CraftingInventory inv = (CraftingInventory) Inventory.Get(inventoryIds[0]);
         ItemStack resultItem = inv.GetItem(inv.GetCraftingResultSlot());
         ItemStack newPointerItem = pointerItem;
 
+        //Cancel if result slot is empty
         if (resultItem.material == Material.Air)
             return;
+        //Cancel if pointer and result slot materials dont match
         if (resultItem.material != pointerItem.material &&
             pointerItem.material != Material.Air)
             return;
+        //Cancel if pointer slot is full
         if (newPointerItem.amount >= Inventory.MaxStackSize)
             return;
-
+        
         //Set pointer material to result material
         newPointerItem.material = resultItem.material;
-
-        while (resultItem.amount > 0 && //Keep moving items until result slot is empty
-               newPointerItem.Amount < Inventory.MaxStackSize)  //Stop if pointer amount exceeds 64
+        
+        //Keep moving items until result slot is empty or if pointer amount exceeds 64
+        while (resultItem.amount > 0 && newPointerItem.Amount < Inventory.MaxStackSize) 
         {
             newPointerItem.Amount += 1;
             resultItem.amount -= 1;
@@ -81,6 +96,45 @@ public class CraftingInventoryMenu : ContainerInventoryMenu
             inv.SetItem(i, newCraftingSlotItem);
         }
 
+        //Update Inventory
+        UpdateInventory();
+    }
+    
+    [Server]
+    public virtual void SlotAction_TransferResultSlotUntilEmpty()
+    {
+        CraftingInventory craftingInventory = (CraftingInventory) Inventory.Get(inventoryIds[0]);
+        PlayerInventory playerInventory = (PlayerInventory) Inventory.Get(inventoryIds[1]);
+
+        while (true)
+        {
+            ItemStack resultItem = craftingInventory.GetItem(craftingInventory.GetCraftingResultSlot());
+            
+            //Stop transferring if result slot is empty
+            if (resultItem.material == Material.Air)
+                break;
+            
+            //Subtract items from recipe slots
+            for (int i = 0; i <= 8; i++)
+            {
+                ItemStack newCraftingSlotItem = craftingInventory.GetItem(i);
+                if(newCraftingSlotItem.material == Material.Air)
+                    continue;
+            
+                newCraftingSlotItem.Amount--;
+                craftingInventory.SetItem(i, newCraftingSlotItem);
+            }
+                
+            //Clear result slot
+            craftingInventory.SetItem(craftingInventory.GetCraftingResultSlot(), new ItemStack());
+            
+            //Add result item to player inventory
+            playerInventory.AddItem(resultItem);
+            
+            //Refresh Crafting slot for next iteration
+            RefreshCraftingResultSlot();
+        }
+        
         //Update Inventory
         UpdateInventory();
     }
