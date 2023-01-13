e0a281f746e01ff64484af679b0145f4412df86f
Birger E <bibbevidio@gmail.com>
2023-01-13 17:21:54 +0100

    Audio mixer manager

---
+++ Assets/AudioMixerManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMixerManager : MonoBehaviour
+{
+    public static AudioMixerManager instance;
+    public AudioMixer mixer;
+
+    public float debugVolume;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(debugVolume) * 20);
+    }
+}
