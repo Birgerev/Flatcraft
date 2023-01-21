1239bcb9559508f439bbe56aaa72402e9ad7288e
Birger E <bibbevidio@gmail.com>
2023-01-21 17:51:45 +0100

    Settings saving & loading

---
+++ Assets/SettingsManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static SettingsManager instance;
+
+    public static Dictionary<string, string> Values = new Dictionary<string, string>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        
+        LoadSettings();
+        
+        InvokeRepeating(nameof(SaveSettings), 5f, 5f);
+    }
+
+    private void LoadSettings()
+    {
+        string[] lines = File.ReadAllLines(GetPath());
+
+        foreach (var line in lines)
+        {
+            string key = line.Split('=')[0];
+            string value = line.Split('=')[1];
+
+            Values[key] = value;
+        }
+    }
+
+    private void SaveSettings()
+    {
+        List<String> lines = new List<string>();
+        
+        foreach (string keyName in Values.Keys)
+        {
+            string value = Values[keyName];
+            
+            lines.Add(keyName + "=" + value);
+        }
+        
+        File.WriteAllLines(GetPath(), lines);
+    }
+
+    public string GetPath()
+    {
+        return Application.persistentDataPath + "\\options.txt";
+    }
+}
