6fc93a26ce24e3a9408abcaa3950a83e34eed6e5
Birger E <bibbevidio@gmail.com>
2023-04-13 18:07:30 +0200

    Removed deprecated method

--- Assets/Scripts/Managers/Boot.cs
+++ Assets/Scripts/Managers/Boot.cs
@@ -20,17 +20,4 @@ public class Boot : MonoBehaviour
         
         SceneManager.LoadScene("MainMenu");
     }
-
-    private bool CreateNameCheck()
-    {
-        string testingNamePath = Application.persistentDataPath + "\\testingProfile.dat";
-        if(!File.Exists(testingNamePath))
-        {
-            SceneManager.LoadScene("TestingCreateName");
-            return true;
-        }
-        
-        //GameNetworkManager.playerName = File.ReadAllText(testingNamePath);
-        return false;
-    }
 }
