8816155f580f7e624e6695fc243e9d104a7df8a9
Birger E <bibbevidio@gmail.com>
2023-04-23 19:34:05 +0200

    Achievement manager

---
+++ Assets/Scripts/Achievment/AchievementManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Steamworks;
+using UnityEditor;
+using UnityEngine;
+
+public class AchievementManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
