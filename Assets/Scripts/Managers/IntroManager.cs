3ed3cd188a6735680bc1b44713a2db1f8c2076c0
Birger E <bibbevidio@gmail.com>
2023-05-03 23:14:35 +0200

    Update IntroManager.cs

--- Assets/Scripts/Managers/IntroManager.cs
+++ Assets/Scripts/Managers/IntroManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Task = System.Threading.Tasks.Task;
