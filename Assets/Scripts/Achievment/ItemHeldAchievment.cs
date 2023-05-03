1f2d0e4b89d48d3d31ef83abc20aba07410bda9a
Birger E <bibbevidio@gmail.com>
2023-05-03 17:18:25 +0200

    Diamond achievement done

--- Assets/Scripts/Achievment/ItemHeldAchievment.cs
+++ Assets/Scripts/Achievment/ItemHeldAchievment.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class ItemHeldAchievment : Achievement
 {
-    
+    public Material[] requiredItem;
+
+    protected override void TrackingLoop()
+    {
+        PlayerInstance localPlayerInstance = PlayerInstance.localPlayerInstance;
+
+        if (!localPlayerInstance)
+            return;
+
+        Player player = localPlayerInstance.playerEntity?.GetComponent<Player>();
+
+        if (!player)
+            return;
+
+        PlayerInventory inventory = player.GetInventory();
+
+        foreach (Material matAlternative in requiredItem)
+        {
+            if (inventory.Contains(matAlternative))
+            {
+                GrantAchievement();
+                return;
+            }
+        }
+    }
 }
