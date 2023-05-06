dd390e8a50658b3ff65b545dd45b841be51f0d0f
Birger E <bibbevidio@gmail.com>
2023-05-06 18:33:50 +0200

    Dimension Achievement

--- Assets/Scripts/Achievment/VisitDimensionAchievement.cs
+++ Assets/Scripts/Achievment/VisitDimensionAchievement.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class VisitDimensionAchievement : Achievement
 {
-    
+    public Dimension trackedDimension;
+
+    protected override void TrackingLoop()
+    {
+        PlayerInstance localPlayerInstance = PlayerInstance.localPlayerInstance;
+
+        if (!localPlayerInstance)
+            return;
+
+        Player playerEntity = localPlayerInstance.playerEntity?.GetComponent<Player>();
+
+        if (!playerEntity)
+            return;
+        
+        if (playerEntity.Location.dimension == trackedDimension)
+            GrantAchievement();
+    }
 }
