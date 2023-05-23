c3a157f4286ed275bf6acebbf058fbe2b57d84dc
Birger E <bibbevidio@gmail.com>
2023-05-23 23:33:14 +0200

    Rename

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OptionsMenu : MonoBehaviour
+public class SettingsMenu : MonoBehaviour
 {
     public Text fullscreenText;
     [Space]
