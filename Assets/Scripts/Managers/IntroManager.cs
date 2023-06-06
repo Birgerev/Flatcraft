22d7c1b028602ae16f1649773dfe917a3612ad45
Birger E <bibbevidio@gmail.com>
2023-06-06 22:28:13 +0200

    Update IntroManager.cs

--- Assets/Scripts/Managers/IntroManager.cs
+++ Assets/Scripts/Managers/IntroManager.cs
@@ -12,7 +12,6 @@ public class IntroManager : MonoBehaviour
     public Animator logoAnimator;
     public SpriteRenderer fadeBackground;
     
-
     // Start is called before the first frame update
     void Start()
     {
