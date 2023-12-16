3f455306c6697b9a75c426da5e4d96f522490712
Birger E <bibbevidio@gmail.com>
2023-12-16 18:05:33 +0100

    Fixed sapling dupe

--- Assets/Scripts/Blocks/Oak_Sapling.cs
+++ Assets/Scripts/Blocks/Oak_Sapling.cs
@@ -7,7 +7,6 @@ public class Oak_Sapling : Block
     public override float averageRandomTickDuration { get; } = 20 * 60;
     public override bool solid { get; set; } = false;
     public override float breakTime { get; } = 0.01f;
-    public override bool requiresGround { get; } = true;
     public override bool isFlammable { get; } = true;
 
     public override Block_SoundType blockSoundType { get; } = Block_SoundType.Grass;
