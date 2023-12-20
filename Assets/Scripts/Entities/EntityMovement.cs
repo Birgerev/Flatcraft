2809916e7ee91f64dc6ae5b75e401fcbef42f7c4
Birger E <bibbevidio@gmail.com>
2023-12-20 16:14:24 +0100

    Cleaned script

--- Assets/Scripts/Entities/EntityMovement.cs
+++ Assets/Scripts/Entities/EntityMovement.cs
@@ -9,7 +9,7 @@ using Random = UnityEngine.Random;
 public class EntityMovement : NetworkBehaviour
 {
     private const float JumpVelocity = 8.5f;
-    private const float StepSoundFrequencyMultiplier = 1.1f;
+    private const float StepSoundDistance = .3f;
     
     [Header("Movement Properties")] 
     private const float AirDrag = 4.3f;
@@ -19,7 +19,7 @@ public class EntityMovement : NetworkBehaviour
     
     private const float Acceleration = 4f;
     private const float ClimbAcceleration = 55.0f;
-    private const float SwimJumpVelocity = 2f;
+    private const float ExitLiquidJumpVelocity = 2f;
     private const float SwimUpAcceleration = 45.0f;
     
     public float walkSpeed = 4.3f;
@@ -27,119 +27,87 @@ public class EntityMovement : NetworkBehaviour
     [HideInInspector] public float speed;
     
     private float _lastJumpTime;
-    private float _lastStepSoundTime;
+    private Vector2 _lastStepPosition;
     private bool _inLiquidLastFrame;
-    private Vector2 _lastFramePosition;
     
     private LivingEntity _entity;
+    private Rigidbody2D _rb;
 
-    private void Start()
+    private void Awake()
     {
         _entity = GetComponent<LivingEntity>();
+        _rb = GetComponent<Rigidbody2D>();
         speed = walkSpeed;
-    }
-    
-    public virtual void LateUpdate()
-    {
-        _lastFramePosition = transform.position;
-        _inLiquidLastFrame = _entity.isInLiquid;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isServer) return;//TODO call on server, for players too?
+        MovementParticleEffect();
         
-        WalkSoundCheck();
-        ClientMovementParticleEffect();
+        if (!isOwned && !isServer) return;
+        
+        WalkSound();
         ApplyFriction();
         
-        if (_inLiquidLastFrame && !_entity.isInLiquid && GetVelocity().y > 0)
-            SetVelocity(GetVelocity() + new Vector2(0, SwimJumpVelocity));
-    }
-    
-    [Server]
-    public void WalkSoundCheck()
-    {
-        if (Mathf.Abs(_entity.GetVelocity().x) < 0.5f)
-            return;
-        
-        float checkFrequency = StepSoundFrequencyMultiplier / Mathf.Abs(GetVelocity().x);
-        float timeOffset = (float) new System.Random(_entity.uuid.GetHashCode()).NextDouble(); //Uses a static seed (id)
-
-        if (Time.time - _lastStepSoundTime > checkFrequency && _entity.isOnGround)
-        {
-            WalkSound();
-            _lastStepSoundTime = Time.time;
-        }
+        //Exit liquid jump
+        if (_inLiquidLastFrame && !_entity.isInLiquid && _rb.velocity.y > 0)
+            _rb.velocity += new Vector2(0, ExitLiquidJumpVelocity);
     }
 
-    public void ApplyFriction()
+    private void ApplyFriction()
     {
-        if (_entity.isInLiquid)
-            SetVelocity(GetVelocity() * (1 / (1 + LiquidDrag * Time.deltaTime)));
-        if (_entity.isOnClimbable)
-            SetVelocity(GetVelocity() * (1 / (1 + ClimbableFriction * Time.deltaTime)));
         if (!_entity.isInLiquid && !_entity.isOnClimbable && !_entity.isOnGround)
-            SetVelocity(new Vector3(GetVelocity().x * (1 / (1 + AirDrag * Time.deltaTime)), GetVelocity().y));
+            _rb.velocity = new Vector3(_rb.velocity.x * (1 / (1 + AirDrag * Time.deltaTime)), _rb.velocity.y);
+        
+        if (_entity.isInLiquid)
+            _rb.velocity *= 1 / (1 + LiquidDrag * Time.deltaTime);
+        if (_entity.isOnClimbable)
+            _rb.velocity *= 1 / (1 + ClimbableFriction * Time.deltaTime);
         if (!_entity.isInLiquid && !_entity.isOnClimbable && _entity.isOnGround)
-            SetVelocity(GetVelocity() * (1 / (1 + GroundFriction * Time.deltaTime)));
+            _rb.velocity *= 1 / (1 + GroundFriction * Time.deltaTime);
     }
 
     public void Walk(int direction)
     {
         if (!isOwned && !isServer)
             return;
-
-        if (GetVelocity().x < speed && GetVelocity().x > -speed)
-        {
-            float targetXVelocity = 0;
-
-            if (direction == -1)
-                targetXVelocity -= speed;
-            else if (direction == 1)
-                targetXVelocity += speed;
-            else
-                targetXVelocity = 0;
-
-            GetComponent<Rigidbody2D>().velocity +=
-                new Vector2(targetXVelocity * (Acceleration * Time.deltaTime), 0);
-        }
-
+        
         StairCheck(direction);
+        
+        if (Mathf.Abs(_rb.velocity.x) > speed) return;
+
+        GetComponent<Rigidbody2D>().velocity +=
+            new Vector2(direction * speed * Acceleration * Time.deltaTime, 0);
     }
 
     public void Jump()
     {
-        if (!isOwned && !isServer)
-            return;
-
-        if (_entity.isOnGround && Time.time - _lastJumpTime >= 0.3f)
-        {
-            SetVelocity(new Vector2(GetVelocity().x, JumpVelocity));
-            _lastJumpTime = Time.time;
-        }
+        if (!isOwned && !isServer) return;
 
         if (_entity.isInLiquid)
-            SetVelocity(GetVelocity() + new Vector2(0, SwimUpAcceleration * Time.deltaTime));
+            _rb.velocity += new Vector2(0, SwimUpAcceleration * Time.deltaTime);
 
         if (_entity.isOnClimbable)
-            SetVelocity(GetVelocity() + new Vector2(0, ClimbAcceleration * Time.deltaTime));
-    }
+            _rb.velocity += new Vector2(0, ClimbAcceleration * Time.deltaTime);
 
-    public void StairCheck(int direction)
+        if (!_entity.isOnGround) return;
+        if (Time.time - _lastJumpTime < .3f) return;
+        
+        _rb.velocity += new Vector2(0, JumpVelocity);
+        _lastJumpTime = Time.time;
+    }
+    
+    private void StairCheck(int direction)
     {
-        if ((Vector2) transform.position != _lastFramePosition) //Return if player has moved since last frame
-            return;
-        if (!_entity.isOnGround) //Return if player isn't grounded
-            return;
+        //Return if player isn't grounded
+        if (!_entity.isOnGround) return;
 
         //Get block in front of player acording to walk direction
         Block blockInFront = Location.LocationByPosition(
             (Vector2) transform.position + new Vector2(direction * 0.5f, 0)).GetBlock();
 
-        if (blockInFront == null)
-            return;
+        if (blockInFront == null) return;
         
         if (Type.GetType(blockInFront.GetMaterial().ToString()).IsSubclassOf(typeof(Stairs)))
         {
@@ -147,54 +115,52 @@ public class EntityMovement : NetworkBehaviour
             bool rotated_y = blockInFront.location.GetData().GetTag("rotated_y") == "true";
 
             //if the stairs are rotated correctly
-            if (rotated_y == false &&
-                (direction == -1 && rotated_x == false || direction == 1 && rotated_x))
-                transform.position += new Vector3(direction * 0.2f, 1);
+            if (rotated_y) return;
+            if (direction == -1 && rotated_x) return;
+            if (direction == 1 && !rotated_x) return;
+            
+            transform.position += new Vector3(direction * 0.2f, 1);
         }
     }
     
     [Server]
-    public virtual void WalkSound()
+    private void WalkSound()
     {
-        string soundName = "entity/step";
-        Sound.Play(_entity.Location, soundName, SoundType.Entities, 0.8f, 1.2f);
-    }
-    
-    [Client]
-    protected void ClientMovementParticleEffect()
-    {
-        if (!_entity.isOnGround)
-            return;
+        if (!_entity.isOnGround) return;
 
-        float walkParticleChance = 1.5f;
+        if (Vector2.Distance(_lastStepPosition, transform.position) < StepSoundDistance) return;
+        
+        Sound.Play(_entity.Location, "entity/step", SoundType.Entities, 0.8f, 1.2f);
+        _lastStepPosition = transform.position;
+    }
+
+    [Client]
+    private void MovementParticleEffect()
+    {
+        if (!_entity.isOnGround) return;
+
+        float walkParticleChance = 1.5f * Time.deltaTime * Mathf.Abs(_rb.velocity.x);
         System.Random r = new System.Random();
 
-        if (r.NextDouble() < walkParticleChance * Time.deltaTime * Mathf.Abs(GetVelocity().x))
-        {
-            Block blockBeneath = (_entity.Location - new Location(0, 1)).GetBlock();
-            if (blockBeneath == null)
-                return;
+        if (r.NextDouble() > walkParticleChance) return;
+        
+        Block blockBeneath = (_entity.Location - new Location(0, 1)).GetBlock();
+        if (blockBeneath == null) return;
 
-            Particle part = Particle.ClientSpawn();
-            Color[] textureColors = blockBeneath.GetColorsInTexture();
+        Particle part = Particle.ClientSpawn();
+        Color[] textureColors = blockBeneath.GetColorsInTexture();
 
-            part.transform.position = transform.position + new Vector3(0, 0.2f);
-            part.color = textureColors[r.Next(textureColors.Length)];
-            part.doGravity = true;
-            part.velocity = new Vector2(
-                GetVelocity().x * -0.3f,
-                Mathf.Abs(GetVelocity().x) * 1.3f *(float)r.NextDouble());
-            part.maxAge = .4f + (float)r.NextDouble() * .6f;
-        }
+        part.transform.position = transform.position + new Vector3(0, 0.2f);
+        part.color = textureColors[r.Next(textureColors.Length)];
+        part.doGravity = true;
+        part.velocity = new Vector2(
+            _rb.velocity.x * -0.3f,
+            Mathf.Abs(_rb.velocity.x) * 1.3f * (float)r.NextDouble());
+        part.maxAge = .4f + (float)r.NextDouble() * .6f;
     }
     
-    public virtual Vector2 GetVelocity()
+    public virtual void LateUpdate()
     {
-        return GetComponent<Rigidbody2D>().velocity;
-    }
-    
-    public virtual void SetVelocity(Vector2 velocity)
-    {
-        GetComponent<Rigidbody2D>().velocity = velocity;
+        _inLiquidLastFrame = _entity.isInLiquid;
     }
 }
