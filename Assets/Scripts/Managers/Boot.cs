09cbc58441739561d71a312b034acd548961507f
Birger E <bibbevidio@gmail.com>
2024-02-17 15:59:21 +0100

    Check set create name on boot

--- Assets/Scripts/Managers/Boot.cs
+++ Assets/Scripts/Managers/Boot.cs
@@ -13,11 +13,26 @@ public class Boot : MonoBehaviour
     void Start()
     {
         //Option to immediately load test world in editor
+        if (CreateNameCheck()) return;
         if (TryLoadTestWorld()) return;
         
         SceneManager.LoadScene("MainMenu");//Load main menu first, so it is active
         SceneManager.LoadScene("Intro", LoadSceneMode.Additive);
     }
+
+    private bool CreateNameCheck()
+    {
+        string testingNamePath = Application.persistentDataPath + "\\testingProfile.dat";
+        if(!File.Exists(testingNamePath))
+        {
+            SceneManager.LoadScene("TestingCreateName");
+            return true;
+        }
+        
+        SettingsManager.PlayerName = File.ReadAllText(testingNamePath);
+        return false;
+    }
+
     private bool TryLoadTestWorld()
     {
         if (autoloadTestWorld && Application.isEditor)
